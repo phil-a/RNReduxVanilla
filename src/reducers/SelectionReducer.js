97f6427ccd40f0d953aae69580d5cb74996b316d
Philip Alekseev <phil.alekseev@gmail.com>
2016-11-19 17:29:56 -0500

    Implement reducer boilerplate the right way

--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,4 +1,12 @@
-export default (state, action) => {
-  console.log(action);
-  return null;
+export default (state = null, action) => {
+  switch(action.type) {
+
+    case 'select_library':
+      return action.payload;
+
+    // return last state
+    default:
+      return state;
+
+  }
 };
